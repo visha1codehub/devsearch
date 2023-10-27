8e8c737023127e0e1eb4911a6b39c3090dc35b55
Vishal Gupta <itsvishalhack@gmail.com>
2023-10-27 18:30:50 +0530

    some add email varibles in .env

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -119,13 +119,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+# sending email configurations
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'django.devsearch.vishal@gmail.com'
-EMAIL_HOST_PASSWORD = 'skcedninhiigzitx'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
